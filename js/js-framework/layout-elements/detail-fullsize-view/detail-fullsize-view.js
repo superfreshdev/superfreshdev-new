c19d15d7de8f3128ac8fd44e4f171f13bf42b441
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2026-03-23 17:06:32 +0100

    update

--- js/js-framework/layout-elements/detail-fullsize-view/detail-fullsize-view.js
+++ js/js-framework/layout-elements/detail-fullsize-view/detail-fullsize-view.js
@@ -1,4 +1,4 @@
-// console.log("🟨 detail-fullsize-view.js")
+console.log("🟨 detail-fullsize-view.js")
 
 
 /* ---------------------------------------------------------------------- */
@@ -7,15 +7,46 @@
 
 var linkDetails_itProject_1 = document.getElementById("js-link-details-top-it-project-1");
 
+// Test fixed Header
+var header_fixed_body = document.getElementById("header-fixed-body");
+
+// Detail Fullview
+var detail_fullsize_container = document.getElementById("defail-fullsize-top-it-project-1");
+
+// Close Detal View
+var btn_close_fullsize = document.getElementById("close-detail-view");
+
+// Test
+var str_cssStyle_update = "";
+
 /* ---------------------------------------------------------------------- */
 /* Add Dom Element to Click Event
 /* ---------------------------------------------------------------------- */
 
-linkDetails_itProject_1.addEventListener("click", ()=> {
+linkDetails_itProject_1.addEventListener("click", async () => {
 
   doDetailFullsizeView("Details 1: Online Ordering Service");
 
+  /* Update CSS Style */
+  str_cssStyle_update = "display:grid;"
+
+  await async_update_cssStyle( detail_fullsize_container, str_cssStyle_update )
+
+
+
 })
+
+// close full size view
+btn_close_fullsize.addEventListener("click", async () => {
+
+  /* Update CSS Style */
+  str_cssStyle_update = "display:none;"
+
+  await async_update_cssStyle( detail_fullsize_container, str_cssStyle_update )
+
+
+})
+
 
 /* ---------------------------------------------------------------------- */
 /* Function:
@@ -26,13 +57,48 @@ linkDetails_itProject_1.addEventListener("click", ()=> {
 
     window.alert("🍎:" + strInfo);
 
-
   }
 
 /* ---------------------------------------------------------------------- */
 /* Function:
-/* > Do Push Slide
+/* > Update Css Style
 /* ---------------------------------------------------------------------- */
+
+async function async_update_cssStyle( domElement, cssStyle ) {
+
+  console.log("🟡 async_update_cssStyle: ")
+
+  /* --------------------------------------------------------------- */
+  /* Step 1:
+  /* > Get domElement
+  /* --------------------------------------------------------------- */
+
+  // exist, is not null ?
+  if( domElement === null ) {
+
+    window.alert("❌: Failed | DomElement dont exist, " + domElement );
+
+  } else {
+
+    // Update CSS Style
+    domElement.style.cssText = cssStyle;
+
+    window.alert("🟩: Successful | CSS Style Update to DomElement " );
+
+  }
+
+
+
+  /* --------------------------------------------------------------- */
+  /* Step 2:
+  /* > Update CSS Style domElement
+  /* --------------------------------------------------------------- */
+
+  return new Promise(resolve => {
+    resolve( );
+  })
+
+}
 
 /* ---------------------------------------------------------------------- */
 /* Function:
